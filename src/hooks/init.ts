a5ec3a5059da08585ae3af488552e00fa38fbc6c
Dov Rosenberg <dov@rosenbergmail.com>
2023-11-25 07:17:56 -0500

    activates on tinymce

--- src/hooks/init.ts
+++ src/hooks/init.ts
@@ -11,12 +11,32 @@ async function init(): Promise<void> {
   // initialize settings first, so other things can use them
   updateModuleSettings(new ModuleSettings());
 
-  registerListener();
+  registerKeyListeners();
 }
 
 // register the main listener
-function registerListener() {
+function registerKeyListeners() {
   jQuery(document).on('keydown', '.ProseMirror.editor-content[contenteditable="true"]', onKeydown);
+
+  // MCE editors are inside an iframe :(
+  // it really seems like there should be a better way to do this, but just putting a keydown on 
+  //    iframes didn't work... I don't think the keys are bubbling up that high
+  // ideally there'd be a hook or something that tells us when a new editor is opened, but the only
+  //    one I can find is only called for ProseEditors
+  var observer = new MutationObserver(function(mutations) {
+      mutations.forEach(function(mutation) {
+        for (let i=0; i<mutation.addedNodes.length; i++) {
+          if (mutation.addedNodes[i].nodeName==='IFRAME') {
+            jQuery(mutation.addedNodes[i].contentDocument).on('keydown', 'body#tinymce.mce-content-body[contenteditable="true"]', onKeydown);
+          }
+        }
+      })
+  });
+  observer.observe(document, {
+      subtree: true,
+      childList: true
+  });
+
 }
 
 function onKeydown(event: KeyboardEvent) {
